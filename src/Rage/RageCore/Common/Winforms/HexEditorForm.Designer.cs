38e7aa95926a8fa07f429fa4c6486c7ec917c884
Skylumz <b1tchimbulletproof@gmail.com>
2020-03-28 01:15:16 -0400

    HexEditorForm Improvements

--- src/Rage/RageCore/Common/Winforms/HexEditorForm.Designer.cs
+++ src/Rage/RageCore/Common/Winforms/HexEditorForm.Designer.cs
@@ -29,18 +29,20 @@
         private void InitializeComponent()
         {
             this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.FilePathStatusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.HexTextBox = new System.Windows.Forms.RichTextBox();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.perLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.HexPerLine16Button = new System.Windows.Forms.ToolStripMenuItem();
-            this.FilePathStatusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.HexTextBox = new System.Windows.Forms.RichTextBox();
+            this.SelectionPropertyGrid = new System.Windows.Forms.PropertyGrid();
             this.MainStatusStrip.SuspendLayout();
             this.MainMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,6 +55,12 @@
             this.MainStatusStrip.Size = new System.Drawing.Size(800, 22);
             this.MainStatusStrip.TabIndex = 0;
             this.MainStatusStrip.Text = "MainStatusStrip";
+            // 
+            // FilePathStatusStripLabel
+            // 
+            this.FilePathStatusStripLabel.Name = "FilePathStatusStripLabel";
+            this.FilePathStatusStripLabel.Size = new System.Drawing.Size(133, 17);
+            this.FilePathStatusStripLabel.Text = "FilePathStatusStripLabel";
             // 
             // MainMenuStrip
             // 
@@ -71,28 +79,6 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
-            this.splitContainer1.Name = "splitContainer1";
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.HexTextBox);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 404);
-            this.splitContainer1.SplitterDistance = 579;
-            this.splitContainer1.TabIndex = 2;
-            // 
-            // HexTextBox
-            // 
-            this.HexTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.HexTextBox.Location = new System.Drawing.Point(0, 0);
-            this.HexTextBox.Name = "HexTextBox";
-            this.HexTextBox.Size = new System.Drawing.Size(579, 404);
-            this.HexTextBox.TabIndex = 0;
-            this.HexTextBox.Text = "";
-            // 
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -106,7 +92,7 @@
             this.perLineToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.HexPerLine16Button});
             this.perLineToolStripMenuItem.Name = "perLineToolStripMenuItem";
-            this.perLineToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.perLineToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
             this.perLineToolStripMenuItem.Text = "PerLine";
             // 
             // HexPerLine16Button
@@ -114,14 +100,44 @@
             this.HexPerLine16Button.Checked = true;
             this.HexPerLine16Button.CheckState = System.Windows.Forms.CheckState.Checked;
             this.HexPerLine16Button.Name = "HexPerLine16Button";
-            this.HexPerLine16Button.Size = new System.Drawing.Size(180, 22);
+            this.HexPerLine16Button.Size = new System.Drawing.Size(86, 22);
             this.HexPerLine16Button.Text = "16";
             // 
-            // FilePathStatusStripLabel
+            // splitContainer1
             // 
-            this.FilePathStatusStripLabel.Name = "FilePathStatusStripLabel";
-            this.FilePathStatusStripLabel.Size = new System.Drawing.Size(133, 17);
-            this.FilePathStatusStripLabel.Text = "FilePathStatusStripLabel";
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.HexTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.SelectionPropertyGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 404);
+            this.splitContainer1.SplitterDistance = 579;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // HexTextBox
+            // 
+            this.HexTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HexTextBox.Location = new System.Drawing.Point(0, 0);
+            this.HexTextBox.Name = "HexTextBox";
+            this.HexTextBox.Size = new System.Drawing.Size(579, 404);
+            this.HexTextBox.TabIndex = 0;
+            this.HexTextBox.Text = "";
+            this.HexTextBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.HexTextBox_MouseUp);
+            // 
+            // SelectionPropertyGrid
+            // 
+            this.SelectionPropertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectionPropertyGrid.Location = new System.Drawing.Point(0, 0);
+            this.SelectionPropertyGrid.Name = "SelectionPropertyGrid";
+            this.SelectionPropertyGrid.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            this.SelectionPropertyGrid.Size = new System.Drawing.Size(217, 404);
+            this.SelectionPropertyGrid.TabIndex = 0;
             // 
             // HexEditorForm
             // 
@@ -138,6 +154,7 @@
             this.MainMenuStrip.ResumeLayout(false);
             this.MainMenuStrip.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -156,5 +173,6 @@
         private System.Windows.Forms.ToolStripMenuItem perLineToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem HexPerLine16Button;
         private System.Windows.Forms.ToolStripStatusLabel FilePathStatusStripLabel;
+        private System.Windows.Forms.PropertyGrid SelectionPropertyGrid;
     }
 }
--- src/Rage/RageCore/Common/Winforms/HexEditorForm.cs
+++ src/Rage/RageCore/Common/Winforms/HexEditorForm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace RageCore.Common.Winforms
         private string FilePath { get; set; }
         private string FileName { get { return Path.GetFileName(FilePath); } }
         private byte[] Data { get; set; }
-        
+
         public HexEditorForm(Icon icon, string filePath, byte[] data)
         {
             InitializeComponent();
@@ -27,6 +28,8 @@ namespace RageCore.Common.Winforms
 
             UpdateHexTextBox();
             UpdateForm(icon);
+
+            HexTextBox.Font = new Font(FontFamily.GenericMonospace, HexTextBox.Font.Size); //makes everything aligned 
         }
 
         private void UpdateForm(Icon icon)
@@ -39,17 +42,124 @@ namespace RageCore.Common.Winforms
 
         private void UpdateHexTextBox()
         {
-            if(Data == null) { return; }
+            if (Data == null) { return; }
 
-            var text = ByteArrayToString(Data);
+            HexTextBox.Text = HexUtilties.ByteArrayToString(Data);
+        }
 
-            HexTextBox.Text = text;
+        private void UpdatePropertyGrid()
+        {
+            var selectedText = HexTextBox.SelectedText;
+
+            var selectedBytes = HexUtilties.ConvertHexStringToByteArray(selectedText);
+
+            if (selectedBytes == null) { selectedBytes = new byte[0]; }
+            
+            var hb = new HexUtilties.HexByte(selectedBytes);
+            SelectionPropertyGrid.SelectedObject = hb;
+        }
+        private void HexTextBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (HexTextBox.SelectedText.Replace(" ", string.Empty).Length % 2 == 0) { UpdatePropertyGrid(); }
         }
 
         //move to Utils/HexUtilties.cs?
-        public static string ByteArrayToString(byte[] data, bool withOffsets = true)
+        public static class HexUtilties
         {
-            return data.ToString();
+            public class HexByte
+            {
+                public byte[] Bytes { get; set; }
+                public string Int8 { get { try { return Bytes[0].ToString(); } catch { return "Invalid"; } } } //
+                public string Uint8 { get { try { return Bytes[0].ToString(); } catch { return "Invalid"; } } } //idk if this is the correct way to do this for unsigned or signed
+                public string Int16 { get { try { return BitConverter.ToInt16(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Uint16 { get { try { return BitConverter.ToUInt16(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Int32 { get { try { return BitConverter.ToInt32(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Uint32 { get { try { return BitConverter.ToUInt32(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Int64 { get { try { return BitConverter.ToInt64(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Uint64 { get { try { return BitConverter.ToUInt64(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Float { get { try { return BitConverter.ToSingle(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string Double { get { try { return BitConverter.ToDouble(Bytes, 0).ToString(); } catch { return "Invalid"; } } }
+                public string ASCII { get { try { return Encoding.ASCII.GetString(Bytes); } catch { return "Invalid"; } } }
+                public string Unicode { get { try { return Encoding.Unicode.GetString(Bytes); } catch { return "Invalid"; } } }
+
+                public HexByte(byte[] bs)
+                {
+                    Bytes = bs;
+                }
+            }
+
+            public static string ByteArrayToString(byte[] data, int lineOffset = 16, bool withOffsets = true, bool withDecodedText = true)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                int lineCount = 0;
+                if (withOffsets)
+                {
+                    string os = "Offset  ";
+                    for (int i = 0; i < lineOffset; i++)
+                    {
+                        os += " " + i.ToString("X2");
+                    }
+
+                    if (withDecodedText)
+                    {
+                        os += " Decoded Text";
+                    }
+
+                    sb.AppendLine(os);
+                }
+
+                StringBuilder hexStr = new StringBuilder();
+                StringBuilder texStr = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (i % lineOffset == 0)
+                    {
+                        hexStr.Append(texStr.ToString());
+                        if (i != 0) { sb.AppendLine(hexStr.ToString()); }
+                        hexStr.Clear();
+                        texStr.Clear();
+                        texStr.Append(" ");
+                        if (withOffsets)
+                        {
+                            hexStr.Append(lineCount.ToString("X8"));
+                            lineCount++;
+                        }
+                    }
+                    hexStr.Append(" " + data[i].ToString("X2"));
+                    if (withDecodedText)
+                    {
+                        //dont really like this
+                        if (data[i] == 0) { texStr.Append("."); }
+                        else
+                        {
+                            var cRes = Convert.ToChar(data[i]);
+                            if (char.IsWhiteSpace(cRes)) { cRes = '.'; }
+                            texStr.Append(cRes);
+                        }
+                    }
+                }
+
+                return sb.ToString();
+            }
+
+            //stolen: https://stackoverflow.com/questions/321370/how-can-i-convert-a-hex-string-to-a-byte-array/321404
+            public static byte[] ConvertHexStringToByteArray(string inputString)
+            {
+                var hexString = inputString.Replace(" ", string.Empty);
+                
+                try
+                {
+                    byte[] retval = new byte[hexString.Length / 2];
+                    for (int i = 0; i < hexString.Length; i += 2)
+                        retval[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+                    return retval;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
         }
     }
 }
